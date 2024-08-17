ccb3ea983f035f943dfaaafbea2669f267816548
ChoiYoungChan <40765022+ChoiYoungChan@users.noreply.github.com>
2024-08-17 19:56:59 +0900

    update UE version and project

---
+++ Source/ProjectShooting/ProjectShootingGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectShootingGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectShootingGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjectShootingGameMode();
+};
+
+
+
